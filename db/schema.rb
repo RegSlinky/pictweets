af12bc5f58a9fd4cb922cce8854b83d840e86522
RegSlinky <thesmile0701@gmail.com>
2023-04-06 16:48:42 +0900

    マイページの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_06_072411) do
+ActiveRecord::Schema.define(version: 2023_04_06_074612) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
